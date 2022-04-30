d4f625f3436a73f250ad0c6ea61a82331d4d00cd
Amine karimi <aminekarimi1998@gmail.com>
2022-04-30 15:46:24 +0000

    feat: add categories in the FreeCodeCampItem

--- app/src/main/java/com/zrcoding/hackertab/ui/source/freecodecamp/FreeCodeCamp.kt
+++ app/src/main/java/com/zrcoding/hackertab/ui/source/freecodecamp/FreeCodeCamp.kt
@@ -1,14 +1,15 @@
 package com.zrcoding.hackertab.ui.source.freecodecamp
 
 import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Divider
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
@@ -17,6 +18,7 @@ import com.zrcoding.hackertab.core.CardUiState
 import com.zrcoding.hackertab.ui.shared.CardHeader
 import com.zrcoding.hackertab.ui.shared.Loading
 import com.zrcoding.hackertab.ui.shared.PostTitle
+import com.zrcoding.hackertab.ui.shared.TextWithStartIcon
 
 data class FreeCodeCamp(
     val title: String,
@@ -55,10 +57,23 @@ fun FreeCodeCampCard(freeCodeCampState: CardUiState<List<FreeCodeCamp>>) {
 }
 
 @Composable
-fun FreeCodeCampItem(post:FreeCodeCamp) {
+fun FreeCodeCampItem(post: FreeCodeCamp) {
     Column(
-        modifier = Modifier.padding(16.dp).fillMaxWidth()
+        modifier = Modifier
+            .padding(16.dp)
+            .fillMaxWidth()
     ) {
         PostTitle(title = post.title.trim())
+
+        LazyRow {
+            items(post.categories) { category ->
+                TextWithStartIcon(
+                    text = category,
+                    icon = R.drawable.ic_score,
+                    tint = Color.Gray
+                )
+            }
+
+        }
     }
 }
