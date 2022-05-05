319ed153066053eb481f453e7c4455674fa5ad8e
amine.karimi <amine.karimi@inyad.com>
2022-05-05 18:44:02 +0000

    Added Nunito font

--- app/src/main/java/com/zrcoding/hackertab/ui/theme/Type.kt
+++ app/src/main/java/com/zrcoding/hackertab/ui/theme/Type.kt
@@ -2,11 +2,20 @@ package com.zrcoding.hackertab.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.zrcoding.hackertab.R
 
 // Set of Material typography styles to start with
+val NunitoFont = FontFamily(
+    Font(R.font.nunito_bold),
+    Font(R.font.nunito_italic),
+    Font(R.font.nunito_medium),
+    Font(R.font.nunito_semi_bold)
+)
+
 val Typography = Typography(
     body1 = TextStyle(
         fontFamily = FontFamily.Default,
