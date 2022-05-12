ddc28ec0188d9b71ec21fbe7830f6fcaaed547d5
zouhir <rajdaouizouhir.pro@gmail.com>
2022-05-13 00:00:43 +0100

    refactor: added some ui improvements

--- app/src/main/java/com/zrcoding/hackertab/ui/theme/Color.kt
+++ app/src/main/java/com/zrcoding/hackertab/ui/theme/Color.kt
@@ -2,8 +2,5 @@ package com.zrcoding.hackertab.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val TextWhite = Color(0xFFEEEEEE)
 
