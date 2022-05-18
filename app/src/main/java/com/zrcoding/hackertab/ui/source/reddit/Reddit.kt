34e680262adf75f4a2972a491dd5012dedbd06f2
zouhir <rajdaouizouhir.pro@gmail.com>
2022-05-18 15:32:37 +0100

    refactor: move hardcoded colors to Color.kt

--- app/src/main/java/com/zrcoding/hackertab/ui/source/reddit/Reddit.kt
+++ app/src/main/java/com/zrcoding/hackertab/ui/source/reddit/Reddit.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -13,6 +12,7 @@ import com.zrcoding.hackertab.core.Constants.FAKE_REDDITS
 import com.zrcoding.hackertab.core.toDate
 import com.zrcoding.hackertab.ui.template.SourceItemTemplate
 import com.zrcoding.hackertab.ui.template.TextWithStartIcon
+import com.zrcoding.hackertab.ui.theme.Flamingo
 import com.zrcoding.hackertab.ui.theme.HackertabTheme
 
 data class Reddit(
@@ -46,9 +46,9 @@ fun RedditItem(reddit: Reddit) {
             ) {
                 TextWithStartIcon(
                     text = stringResource(id = R.string.score, reddit.score),
-                    textColor = Color(0xFFf6682f),
+                    textColor = Flamingo,
                     icon = R.drawable.ic_ellipse,
-                    tint = Color(0xFFf6682f)
+                    tint = Flamingo
                 )
                 TextWithStartIcon(
                     text = stringResource(id = R.string.comments, reddit.commentsCount),
