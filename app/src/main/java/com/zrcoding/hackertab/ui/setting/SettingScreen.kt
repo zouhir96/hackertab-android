0c39a73d822c294f2f92eab3dc76da35d1b591ab
Amine karimi <aminekarimi1998@gmail.com>
2022-07-08 01:15:33 +0100

    List sources as chips in the setting screen

--- app/src/main/java/com/zrcoding/hackertab/ui/setting/SettingScreen.kt
+++ app/src/main/java/com/zrcoding/hackertab/ui/setting/SettingScreen.kt
@@ -7,15 +7,23 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.zrcoding.hackertab.R
+import com.zrcoding.hackertab.core.ChipData
+import com.zrcoding.hackertab.core.ChipGroup
 import com.zrcoding.hackertab.ui.theme.HackertabTheme
 
 @SuppressLint("UnrememberedMutableState")
 @Composable
 fun SettingScreen() {
-
+    val selectedChipData: MutableState<ChipData?> = remember {
+        mutableStateOf(null)
+    }
 
     Column(modifier = Modifier) {
         Text(
@@ -28,6 +36,23 @@ fun SettingScreen() {
         Text(
             text = "Programming languages you’re interested in",
             style = MaterialTheme.typography.subtitle1
+        )
+
+        val chipsList = listOf(
+            ChipData("Github", R.drawable.ic_github),
+            ChipData("FreeCodeCamp"),
+            ChipData("HackerNews"),
+            ChipData("DevTo"),
+            ChipData("Reddit"),
+            ChipData("Product Hunt")
+        )
+
+        ChipGroup(
+            chips = chipsList,
+            selectedChip = selectedChipData.value,
+            onSelectedChanged = { selectedChip ->
+                selectedChipData.value = selectedChip
+            }
         )
     }
 }
