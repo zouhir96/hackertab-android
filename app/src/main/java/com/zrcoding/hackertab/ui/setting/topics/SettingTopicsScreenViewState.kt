84777cfc23dcf3a54b9dfbc3d39640748ed19475
zouhir <rajdaouizouhir.pro@gmail.com>
2024-02-10 15:25:32 +0100

    feat: Add topics screen ui and domain layers.

---
+++ app/src/main/java/com/zrcoding/hackertab/ui/setting/topics/SettingTopicsScreenViewState.kt
@@ -0,0 +1,7 @@
+package com.zrcoding.hackertab.ui.setting.topics
+
+import com.zrcoding.hackertab.core.ChipData
+
+data class SettingTopicsScreenViewState(
+    val topics: List<ChipData> = emptyList(),
+)
