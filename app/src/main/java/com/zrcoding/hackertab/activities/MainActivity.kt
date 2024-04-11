30630eaaa703ea4dc1bf7ab5642605062237b8d2
zouhir <rajdaouizouhir.pro@gmail.com>
2024-04-11 22:28:49 +0200

    feat: Removed unused files.

--- app/src/main/java/com/zrcoding/hackertab/activities/MainActivity.kt
+++ app/src/main/java/com/zrcoding/hackertab/activities/MainActivity.kt
@@ -16,9 +16,6 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
-            window.statusBarColor = Color.BLACK
-        }
         installSplashScreen()
 
         super.onCreate(savedInstanceState)
--- app/src/main/java/com/zrcoding/hackertab/core/UiText.kt
+++
@@ -1,6 +0,0 @@
-package com.zrcoding.hackertab.core
-
-sealed class UiText {
-    class Message(val message: String):UiText()
-    class Code(val code: Int):UiText()
-}
