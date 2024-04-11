ac43f9896872d2202a9e108296fab9b31bfe1738
zouhir <rajdaouizouhir.pro@gmail.com>
2024-04-11 22:28:05 +0200

    feat: Created design module.

---
+++ design/build.gradle.kts
@@ -0,0 +1,51 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.android.kotlin)
+}
+
+android {
+    namespace = "com.zrcoding.hackertab.design"
+
+    compileSdk = libs.versions.compileSdk.get().toInt()
+
+    defaultConfig {
+        minSdk = libs.versions.minSdk.get().toInt()
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        libs.versions.jvmTarget.get()
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = libs.versions.depComposeCompiler.get()
+    }
+}
+
+dependencies {
+    // UI
+    api(platform(libs.androidx.compose.bom))
+    api(libs.androidx.compose.ui)
+    api(libs.androidx.compose.ui.tooling.preview)
+    api(libs.androidx.compose.material)
+    api(libs.io.coil.compose)
+    api(libs.io.coil.gif)
+
+    // Tools
+    debugApi(libs.androidx.compose.ui.tooling)
+}
